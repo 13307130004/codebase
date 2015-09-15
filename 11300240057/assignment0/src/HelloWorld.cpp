3ab18f872ac5db91e4b45ca15ed800021da394a8
13307130004 <13307130004@fudan.edu.cn>
2015-09-15 14:52:11 +0800

    helloworld! commit by CL

--- 11300240057/assignment0/src/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstdio>
-
-using namespace std;
-
-int main(){
-    cout << a << endl;
-    return 0;
-}
--- 13307130444/assignment0/src/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-using namespace std;
-
-int main()
-{
-	printf("hello!");
-	return 0;
-}
